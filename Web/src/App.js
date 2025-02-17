65249129687eecd8298a1c80641115f82b4bdf9e
Nikhil Mantha <nikhilmantha@nikhils-mbp.mynetworksettings.com>
2025-02-16 20:06:59 -0800

    02/16/2025

--- Web/src/App.js
+++ Web/src/App.js
@@ -1,3 +1,43 @@
+import { useEffect, useState } from 'react';
+
 export default function Restaurant() {
-  return <>Hello World!</>;
+  console.log('Restaurant component');
+  useEffect(() => {
+     fetch('https://6lw52i992c.execute-api.us-west-2.amazonaws.com/prod/menu')
+       .then(async response => setMenu(await response.json()))
+       .catch(error => console.log(error));
+  }, []);
+
+  const [menu, setMenu] = useState([
+    { id: 0, name: 'Pizza', price: 10, category: 'Main' },
+    { id: 1, name: 'Salad', price: 5, category: 'Appetizer' },
+    { id: 2, name: 'Soda', price: 2, category: 'Beverage' }
+  ]);
+  return (
+    <>
+      <h1>Restaurant</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {menu.map(mi => <MenuItem item={mi} />)}
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+function MenuItem({ item }) {
+  return (
+    <tr key={item.id}>
+      <td>{item.name}</td>
+      <td>${item.price}.00</td>
+      <td>{item.category}</td>
+    </tr>
+  );
 }
